f517a8e0432626cce7fe8e9a69a6188e1f220d70
Leonardo <leonardo123k@gmail.com>
2019-10-20 18:59:00 -0300

    Atualizado a configuraçao do sequelize

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
 const express = require('express')
-const db = require('./models/index')
+const routes = require('./routes')
 
 const app = express()
-db.authenticate()
-  .then(() => {
-    console.log('Connection stablished')
-  })
-  .catch(err => {
-    console.error('Unable to connect to database: ', err)
-  })
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Servidor rodando na porta 3333')
