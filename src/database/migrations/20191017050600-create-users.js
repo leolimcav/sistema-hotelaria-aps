82166a1fafb0e0c4ce107a5ab17b496fcb7c9c76
Leonardo <leonardo123k@gmail.com>
2019-10-31 20:07:15 -0300

    added jest lib for testing and tests folder

--- src/database/migrations/20191017050600-create-users.js
+++ src/database/migrations/20191017050600-create-users.js
@@ -12,6 +12,10 @@ module.exports = {
       nome: {
         type: Sequelize.STRING,
         allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
       }
     })
   },
